526bf0931c5f2a62ccee46e49f158d3f390c2c92
naresh-khatri <naresh.khatri2345@gmail.com>
2022-08-06 23:51:12 +0530

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-
+import { Route, Routes } from "react-router-dom";
 import { Box, Stack, Skeleton } from "@chakra-ui/react";
 import {
   Button,
@@ -13,6 +13,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 
+import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import AlertExample from "./components/AlertExample";
 import ToolBar from "./components/ToolBar";
@@ -46,92 +47,100 @@ function App() {
     } catch (error) {
       setIsLoading(false);
       console.log(error);
-      setOutput = "Somehings fishy :thinking_face:";
+      setOutput("Somehings fishy :thinking_face:");
     }
   };
   return (
     <div className="App">
-      <Header />
-      {/* <AlertExample /> */}
-      <ToolBar
-        isLoading={isLoading}
-        runCode={runCode}
-        setLang={setLang}
-        setTheme={setTheme}
-      />
-      <Box h="800px">
-        <Split
-          className="split"
-          minSize={10}
-          maxSize={1000}
-          gutterSize={13}
-          gutterAlign="center"
-          snapOffset={10}
-          dragInterval={10}
-        >
-          <Box>
-            <CodeEditor
-              code={code}
-              setCode={setCode}
-              lang={lang}
-              theme={theme}
-            />
-          </Box>
-          <Box>
-            {" "}
-            {isLoading && (
-              <Stack>
-                <Skeleton
-                  height="20px"
-                  fadeDuration={1}
-                  isLoaded={!isLoading}
-                />
-                <Skeleton
-                  height="20px"
-                  fadeDuration={3}
-                  isLoaded={!isLoading}
-                />
-                <Skeleton
-                  height="20px"
-                  fadeDuration={5}
-                  isLoaded={!isLoading}
-                />
-              </Stack>
-            )}
-            {!isLoading && (
-              <Box>
-                <OutputViewer output={output} theme={theme} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <ToolBar
+                isLoading={isLoading}
+                runCode={runCode}
+                setLang={setLang}
+                setTheme={setTheme}
+              />
+              <Box h="800px">
+                <Split
+                  className="split"
+                  minSize={10}
+                  maxSize={1000}
+                  gutterSize={13}
+                  gutterAlign="center"
+                  snapOffset={10}
+                  dragInterval={10}
+                >
+                  <Box>
+                    <CodeEditor
+                      code={code}
+                      setCode={setCode}
+                      lang={lang}
+                      theme={theme}
+                    />
+                  </Box>
+                  <Box>
+                    {isLoading && (
+                      <Stack bg="gray.800" h="100%">
+                        <Skeleton
+                          height="20px"
+                          fadeDuration={1}
+                          isLoaded={!isLoading}
+                        />
+                        <Skeleton
+                          height="20px"
+                          fadeDuration={3}
+                          isLoaded={!isLoading}
+                        />
+                        <Skeleton
+                          height="20px"
+                          fadeDuration={5}
+                          isLoaded={!isLoading}
+                        />
+                      </Stack>
+                    )}
+                    {!isLoading && (
+                      <Box>
+                        <OutputViewer output={output} theme={theme} />
+                      </Box>
+                    )}
+                  </Box>
+                </Split>
               </Box>
-            )}
-          </Box>
-        </Split>
-      </Box>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              MASTERRRRRRRRRRRR
-            </AlertDialogHeader>
+              <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+              >
+                <AlertDialogOverlay>
+                  <AlertDialogContent>
+                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                      MASTERRRRRRRRRRRR
+                    </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Are you sure? You can't undo this action afterwards.
-            </AlertDialogBody>
+                    <AlertDialogBody>
+                      Are you sure? You can't undo this action afterwards.
+                    </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose} mr={4}>
-                Cancel
-              </Button>
-              <Button colorScheme="red" onClick={onClose}>
-                Delete
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+                    <AlertDialogFooter>
+                      <Button ref={cancelRef} onClick={onClose} mr={4}>
+                        Cancel
+                      </Button>
+                      <Button colorScheme="red" onClick={onClose}>
+                        Delete
+                      </Button>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialogOverlay>
+              </AlertDialog>
+            </>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
