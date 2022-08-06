901eb65248c918720a0ae6e6b3ba03eb7b0f6e48
naresh-khatri <naresh.khatri2345@gmail.com>
2022-08-06 23:51:12 +0530

    Toolbar lets you select themes and languages

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,46 +1,55 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button, HStack, Select } from "@chakra-ui/react";
+import { Button, Box, HStack, Select } from "@chakra-ui/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function ToolBar({ isLoading, runCode, setLang, setTheme }) {
   return (
-    <HStack m={2} justifyContent="end">
-      <Select
-        maxW={40}
-        onChange={(e) => {
-          setLang(e.target.value);
-        }}
-      >
-        <option value="python">Python</option>
-        <option value="js">Javascript</option>
-        <option value="cpp">C++</option>
-        <option value="c">C</option>
-        <option value="java">Java</option>
-      </Select>
-      <Select maxW={40} onChange={(e) => setTheme(e.target.value)}>
-        <option value="dracula">Dracula</option>
-        <option value="atomone">Atom One</option>
-        <option value="eclipse">Eclipse</option>
-        <option value="okaidia">Okaidia</option>
-        <option value="githubDark">Github Dark</option>
-        <option value="githubLight">Github Light</option>
-        <option value="duotoneDark">Duotone Dark</option>
-        <option value="duotoneLight">Duotone Light</option>
-        <option value="xcodeDark">Xcode Dark</option>
-        <option value="xcodeLight">Xcode Light</option>
-      </Select>
-      <Button
-        loadingText="Running..."
-        isLoading={isLoading}
-        bg="green.400"
-        leftIcon={<FontAwesomeIcon icon={faPlay} />}
-        color="white"
-        onClick={runCode}
-      >
-        Run
-      </Button>
-    </HStack>
+    <Box bg="gray.800">
+      <HStack p={2} justifyContent="end">
+        <Select
+          bg="purple.500"
+          color="white"
+          maxW={40}
+          onChange={(e) => {
+            setLang(e.target.value);
+          }}
+        >
+          <option style={{color:'black'}} value="python">Python</option>
+          <option style={{color:'black'}} value="js">Javascript</option>
+          <option style={{color:'black'}} value="cpp">C++</option>
+          <option style={{color:'black'}} value="c">C</option>
+          <option style={{color:'black'}} value="java">Java</option>
+        </Select>
+        <Select
+          maxW={40}
+          onChange={(e) => setTheme(e.target.value)}
+          bg="purple.500"
+          color="white"
+        >
+          <option style={{color:'black'}} value="dracula">Dracula</option>
+          <option style={{color:'black'}} value="atomone">Atom One</option>
+          <option style={{color:'black'}} value="eclipse">Eclipse</option>
+          <option style={{color:'black'}} value="okaidia">Okaidia</option>
+          <option style={{color:'black'}} value="githubDark">Github Dark</option>
+          <option style={{color:'black'}} value="githubLight">Github Light</option>
+          <option style={{color:'black'}} value="duotoneDark">Duotone Dark</option>
+          <option style={{color:'black'}} value="duotoneLight">Duotone Light</option>
+          <option style={{color:'black'}} value="xcodeDark">Xcode Dark</option>
+          <option style={{color:'black'}} value="xcodeLight">Xcode Light</option>
+        </Select>
+        <Button
+          loadingText="Running..."
+          isLoading={isLoading}
+          bg="green.400"
+          leftIcon={<FontAwesomeIcon icon={faPlay} />}
+          color="white"
+          onClick={runCode}
+        >
+          Run
+        </Button>
+      </HStack>
+    </Box>
   );
 }
