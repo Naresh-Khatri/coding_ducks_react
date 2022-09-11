9a839701a3680e34f6555f859932319b61c442b6
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-12 00:35:52 +0530

    rearrange button and select components

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,5 +1,5 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button, Box, HStack, Select } from "@chakra-ui/react";
+import { Button, Box, HStack, Select, Spacer } from "@chakra-ui/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
@@ -8,37 +8,7 @@ export default function ToolBar({ isLoading, runCode, setLang, setTheme }) {
   return (
     <Box bg="gray.800">
       <HStack p={2} justifyContent="end">
-        <Select
-          bg="purple.500"
-          color="white"
-          maxW={40}
-          onChange={(e) => {
-            setLang(e.target.value);
-          }}
-        >
-          <option style={{color:'black'}} value="python">Python</option>
-          <option style={{color:'black'}} value="js">Javascript</option>
-          <option style={{color:'black'}} value="cpp">C++</option>
-          <option style={{color:'black'}} value="c">C</option>
-          <option style={{color:'black'}} value="java">Java</option>
-        </Select>
-        <Select
-          maxW={40}
-          onChange={(e) => setTheme(e.target.value)}
-          bg="purple.500"
-          color="white"
-        >
-          <option style={{color:'black'}} value="dracula">Dracula</option>
-          <option style={{color:'black'}} value="atomone">Atom One</option>
-          <option style={{color:'black'}} value="eclipse">Eclipse</option>
-          <option style={{color:'black'}} value="okaidia">Okaidia</option>
-          <option style={{color:'black'}} value="githubDark">Github Dark</option>
-          <option style={{color:'black'}} value="githubLight">Github Light</option>
-          <option style={{color:'black'}} value="duotoneDark">Duotone Dark</option>
-          <option style={{color:'black'}} value="duotoneLight">Duotone Light</option>
-          <option style={{color:'black'}} value="xcodeDark">Xcode Dark</option>
-          <option style={{color:'black'}} value="xcodeLight">Xcode Light</option>
-        </Select>
+        <Box></Box>
         <Button
           loadingText="Running..."
           isLoading={isLoading}
@@ -49,6 +19,67 @@ export default function ToolBar({ isLoading, runCode, setLang, setTheme }) {
         >
           Run
         </Button>
+        <Select
+          bg="purple.500"
+          color="white"
+          maxW={40}
+          onChange={(e) => {
+            setLang(e.target.value);
+          }}
+        >
+          <option style={{ color: "black" }} value="python">
+            Python
+          </option>
+          <option style={{ color: "black" }} value="js">
+            Javascript
+          </option>
+          <option style={{ color: "black" }} value="cpp">
+            C++
+          </option>
+          <option style={{ color: "black" }} value="c">
+            C
+          </option>
+          <option style={{ color: "black" }} value="java">
+            Java
+          </option>
+        </Select>
+        <Select
+          maxW={40}
+          onChange={(e) => setTheme(e.target.value)}
+          bg="purple.500"
+          color="white"
+        >
+          <option style={{ color: "black" }} value="dracula">
+            Dracula
+          </option>
+          <option style={{ color: "black" }} value="atomone">
+            Atom One
+          </option>
+          <option style={{ color: "black" }} value="eclipse">
+            Eclipse
+          </option>
+          <option style={{ color: "black" }} value="okaidia">
+            Okaidia
+          </option>
+          <option style={{ color: "black" }} value="githubDark">
+            Github Dark
+          </option>
+          <option style={{ color: "black" }} value="githubLight">
+            Github Light
+          </option>
+          <option style={{ color: "black" }} value="duotoneDark">
+            Duotone Dark
+          </option>
+          <option style={{ color: "black" }} value="duotoneLight">
+            Duotone Light
+          </option>
+          <option style={{ color: "black" }} value="xcodeDark">
+            Xcode Dark
+          </option>
+          <option style={{ color: "black" }} value="xcodeLight">
+            Xcode Light
+          </option>
+        </Select>
       </HStack>
     </Box>
   );
