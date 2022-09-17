c728fe3e9043758e594e70bab61a7009796172c7
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-17 08:39:59 +0530

    add an option to rename the file and use select component option value python -> py

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,18 +1,83 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button, Box, HStack, Select, Spacer } from "@chakra-ui/react";
+import {
+  Button,
+  Box,
+  HStack,
+  Select,
+  Spacer,
+  IconButton,
+  Text,
+  Input,
+  InputRightElement,
+  InputGroup,
+} from "@chakra-ui/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faEdit, faSave } from "@fortawesome/free-solid-svg-icons";
 
-export default function ToolBar({ isLoading, runCode, lang, setLang, theme, setTheme, saveUserFile, saveBtnLoading }) {
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+export default function ToolBar({
+  isLoading,
+  runCode,
+  lang,
+  setLang,
+  theme,
+  setTheme,
+  saveUserFile,
+  saveBtnLoading,
+}) {
+  const { currentFile } = useContext(UserContext);
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [newFileName, setNewFileName] = useState(currentFile.fileName);
+  const handleRenameClick = () => {
+    console.log("rename");
+    setIsRenaming(false);
+  };
   return (
     <Box bg="gray.800">
       <HStack p={2} justifyContent="end">
-        <Box></Box>
+        <Box color="white">
+          {isRenaming ? (
+            <InputGroup size="md">
+              <Input
+                placeholder="File Name"
+                value={newFileName}
+                onChange={(e) => {
+                  setNewFileName(e.target.value);
+                }}
+              />
+              <InputRightElement width="4.5rem">
+                <IconButton
+                  bg="transparent"
+                  color="white"
+                  icon={<FontAwesomeIcon icon={faSave} />}
+                  size="sm"
+                  onClick={handleRenameClick}
+                >Save</IconButton>
+              </InputRightElement>
+            </InputGroup>
+          ) : (
+            <HStack>
+              <Text fontSize="xl">{currentFile.fileName}</Text>
+              <IconButton
+                bg="transparent"
+                _hover={{ bg: "transparent" }}
+                _focus={{ bg: "transparent" }}
+                onClick={() => setIsRenaming(true)}
+                icon={<FontAwesomeIcon icon={faEdit} />}
+              />
+            </HStack>
+          )}
+        </Box>
         <Button
-        isLoading={saveBtnLoading}
-        loadingText="Syncing..."
-         onClick={saveUserFile}> save</Button>
+          isLoading={saveBtnLoading}
+          loadingText="Syncing..."
+          onClick={saveUserFile}
+        >
+          save
+        </Button>
 
         <Button
           loadingText="Running..."
@@ -28,24 +93,24 @@ export default function ToolBar({ isLoading, runCode, lang, setLang, theme, setT
           bg="purple.500"
           color="white"
           maxW={40}
-          value = {lang}
+          value={lang}
           onChange={(e) => {
             setLang(e.target.value);
           }}
         >
-          <option style={{ color: "black" }} value="python" >
+          <option style={{ color: "black" }} value="py">
             Python
           </option>
-          <option style={{ color: "black" }} value="js" >
+          <option style={{ color: "black" }} value="js">
             Javascript
           </option>
-          <option style={{ color: "black" }} value="cpp" >
+          <option style={{ color: "black" }} value="cpp">
             C++
           </option>
-          <option style={{ color: "black" }} value="c" >
+          <option style={{ color: "black" }} value="c">
             C
           </option>
-          <option style={{ color: "black" }} value="java" >
+          <option style={{ color: "black" }} value="java">
             Java
           </option>
         </Select>
